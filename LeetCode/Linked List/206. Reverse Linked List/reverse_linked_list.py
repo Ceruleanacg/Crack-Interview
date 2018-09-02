244e89d0f497a08898fa06263d1038885d72675d
Shuyu <ceruleanacg@gmail.com>
2018-09-03 00:25:45 +0800

    - Reverse Linked List

--- LeetCode/Linked List/206. Reverse Linked List/reverse_linked_list.py
+++ LeetCode/Linked List/206. Reverse Linked List/reverse_linked_list.py
@@ -1,10 +1,9 @@
 # https://leetcode.com/problems/reverse-linked-list/description/
 
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 class Solution:
     def reverseList(self, head):
@@ -12,12 +11,41 @@ class Solution:
         :type head: ListNode
         :rtype: ListNode
         """
-        if not head:
-            return head
+        prev = None
+        while head:
+            cur = head
+            head = head.next
+            cur.next = prev
+            prev = cur
+        return prev
 
-        cur_node = head
-        nex_node = head.next
+    # def reverseList(self, head, prev=None):
+    #     """
+    #     :type head: ListNode
+    #     :rtype: ListNode
+    #     """
+    #     if not head:
+    #         return prev
+    #     next = head.next
+    #     head.next = prev
+    #     return self.reverseList(next, head)
 
-        while nex_node:
-            # 1. Cache nex_node.next.
-            cache_nex_node_next
+
+a = ListNode(1)
+b = ListNode(2)
+c = ListNode(3)
+d = ListNode(4)
+e = ListNode(5)
+
+a.next = b
+b.next = c
+c.next = d
+d.next = e
+e.next = None
+
+
+p = Solution().reverseList(a)
+
+while p.next:
+    print(p.val)
+    p = p.next
